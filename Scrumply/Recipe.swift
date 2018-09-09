6042501f3e01e9e46c153a93667517faa6a71a0f
Jinara <jinaraweerakoon@gmail.com>
2018-09-09 19:40:27 -0400

    I compacted 2 recipes

--- Scrumply/Recipe.swift
+++ Scrumply/Recipe.swift
@@ -28,40 +28,10 @@ class Recipe {
 
 var breakfastRecipe1 = Recipe(name: "Oatmeal", ingredients: ["1 Cup of Old-Fashioned Oats", "Milk", "1 Cup Water", "1/8 Teaspoon Kosher Salt"], steps: ["Combine oats, milk, water, salt, and cinnamon in a medium saucepan.", "Bring to a boil, then reduce heat to low.", "Simmer uncovered for 3 to 5 minutes until thickened, stirring occasionally.", "Remove from heat and let cool slightly.", "Divide equally between two bowls.", "Drizzle each serving with 1/2 teaspoon honey.", "Add additional desired toppings and serve."], mealType: "Breakfast", servingSize: 1)
 
+var breakfastRecipe2 = Recipe(name: "Scrambled Eggs and Toast", ingredients: ["Two Eggs", "One Slice of Bread", "Optional Toppings"], steps: ["Beat eggs in a bowl till blended.","Heat a large nonstick skillet over medium heat until hot.", "Pour in the blended eggs.","Gently pull the eggs across the pan with an inverted turner, forming large soft curds." , "Continue cooking – pulling, lifting and folding eggs – until thickened and no visible liquid egg remains."," Do not stir constantly.","Remove from heat.","Toast bread until golden brown.","Pour the scrambled eggs on to toast."], mealType: "Breakfast", servingSize: 6)
 
-class breakfastRecipe2 {
-    // Mark Properties
-    var name: String
-    var ingredients: [String]
-    var steps: [String]
-    var mealType: String
-    
-    //Mark: Methods
-    init(name: String, ingredients: [String], steps: [String], breakfast: [String]) {
-        self.name = "Scrambled Eggs and Toast"
-        self.ingredients = ["Two Eggs", "One Slice of Bread", "Optional Toppings"]
-        self.steps = ["Beat eggs in a bowl till blended.","Heat a large nonstick skillet over medium heat until hot.", "Pour in the blended eggs.","Gently pull the eggs across the pan with an inverted turner, forming large soft curds." , "Continue cooking – pulling, lifting and folding eggs – until thickened and no visible liquid egg remains."," Do not stir constantly.","Remove from heat.","Toast bread until golden brown.","Pour the scrambled eggs on to toast.","Enjoy!"]
-        self.mealType = "Breakfast"
+var breakfastRecipe3 = Recipe(name: "Crepes", ingredients: ["Two Eggs", "One Cup of All Purpose Flour", "1/2 Cup of Milk" , "1/2 Cup of Water", "Two Tablespoons of Butter"], steps: ["In a large mixing bowl, whisk together the flour and the eggs." , "Gradually add in the milk and water, stirring to combine." , "Add the salt and butter; beat until smooth." , "Heat a lightly oiled griddle or frying pan over medium high heat." , "Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each crepe." , "Tilt the pan with a circular motion so that the batter coats the surface evenly." , "Cook the crepe for about 2 minutes, until the bottom is light brown." , "Loosen with a spatula, turn and cook the other side." , "Add fresh fruit or other toppings to the inside, then fold both sides in." , "Enjoy warm."], mealType: "Breakfast", servingSize: 1)
 
-    }
-}
-class breakfastRecipe3{
-    // Mark Properties
-    var name: String
-    var ingredients: [String]
-    var steps: [String]
-    var mealType: String
-    
-    //Mark: Methods
-    init(name: String, ingredients: [String], steps: [String], breakfast: [String]) {
-        self.name = "Crepes"
-        self.ingredients = ["Two Eggs", "One Cup of All Purpose Flour", "1/2 Cup of Milk" , "1/2 Cup of Water", "Two Tablespoons of Butter"]
-        self.steps = ["In a large mixing bowl, whisk together the flour and the eggs." , "Gradually add in the milk and water, stirring to combine." , "Add the salt and butter; beat until smooth." , "Heat a lightly oiled griddle or frying pan over medium high heat." , "Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each crepe." , "Tilt the pan with a circular motion so that the batter coats the surface evenly." , "Cook the crepe for about 2 minutes, until the bottom is light brown." , "Loosen with a spatula, turn and cook the other side." , "Add fresh fruit or other toppings to the inside, then fold both sides in." , "Enjoy warm."]
-        
-        self.mealType = "Breakfast"
-        
-    }
-}
 class breakfastRecipe4{
     // Mark Properties
     var name: String
